eea3f4ae1a5b2ea88f599d20bfc10d396b8253ee
KudeKube <Kuutti.taavitsainen@icloud.com>
2024-04-19 16:15:17 +0300

    Initial Commit

---
+++ Waves/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Waves
+//
+//  Created by Kuutti Taavitsainen on 19.4.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
